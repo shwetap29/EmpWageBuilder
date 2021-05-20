1df00a29ddc083408ca6d721d97a59812b26298c
shwetap29 <shwetaprasad48@gmail.com>
2021-05-20 09:26:00 +0530

    Added: code to calculate wage for employee

--- src/main/java/com/bridgelabz/EmpWagebuilder.java
+++ src/main/java/com/bridgelabz/EmpWagebuilder.java
@@ -3,13 +3,15 @@ package com.bridgelabz;
 public class EmpWagebuilder {
     public static void main(String[] args) {
         int IS_PRESENT= 1;
+        final int WAGE_PER_HR = 20;
+        int empHR =0;
         double empCheck = (Math.random() * 10) % 2;
+        System.out.println(IS_PRESENT);
          if (empCheck == IS_PRESENT) {
-             System.out.println("employee is present");
-         }else{
-             System.out.println("employee is absent");
-         }
-
+             empHR= 8;
+                 }
+         int empWage = WAGE_PER_HR * empHR;
+         System.out.println("Employoee wage:" + empWage)
         }
 
 }
