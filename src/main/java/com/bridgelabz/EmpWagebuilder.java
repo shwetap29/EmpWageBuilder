f64bb6c78f4c33e8e05770927cfb3366b94825f4
shwetap29 <shwetaprasad48@gmail.com>
2021-05-20 10:10:19 +0530

    REFACTORED: changed code to use switch case statement

--- src/main/java/com/bridgelabz/EmpWagebuilder.java
+++ src/main/java/com/bridgelabz/EmpWagebuilder.java
@@ -1,17 +1,37 @@
 package com.bridgelabz;
 
 public class EmpWagebuilder {
-    public static void main(String[] args) {
-        int IS_PRESENT= 1;
-        final int WAGE_PER_HR = 20;
-        int empHR =0;
-        double empCheck = (Math.random() * 10) % 2;
-        System.out.println(IS_PRESENT);
-         if (empCheck == IS_PRESENT) {
-             empHR= 8;
-                 }
-         int empWage = WAGE_PER_HR * empHR;
-         System.out.println("Employoee wage:" + empWage)
-        }
+        private static final int IS_FULL_TIME = 1;
+        private static final int WAGE_PER_HR = 20;
+        private static final int IS_PART_TIME = 2;
 
-}
+        public static void main(String[] args) {
+
+            int empHR = 0;
+
+            double empCheck = (Math.random() * 10) % 3;
+            System.out.println(IS_FULL_TIME);
+            // full time
+            // if (empCheck == IS_FULL_TIME) {
+            //    empHR= 8;
+            //      }
+            // part time
+            //  if (empCheck == IS_PART_TIME) {
+            //    empHR= 4;
+            // }
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHR = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHR = 4;
+                    break;
+                default:
+                    empHR = 0;
+                    break;
+            }
+            int empWage = WAGE_PER_HR * empHR;
+            System.out.println("Employee wage:" + empWage);
+        }
+    }
+
