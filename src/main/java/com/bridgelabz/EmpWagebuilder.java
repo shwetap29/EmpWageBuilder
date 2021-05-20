e6ff5b8fbe5e54ccfe8c42360b34aef60d0df1f5
shwetap29 <shwetaprasad48@gmail.com>
2021-05-20 09:03:48 +0530

    modified the code and added empcheck

--- src/main/java/com/bridgelabz/EmpWagebuilder.java
+++ src/main/java/com/bridgelabz/EmpWagebuilder.java
@@ -2,6 +2,14 @@ package com.bridgelabz;
 
 public class EmpWagebuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to emp wage computation program");
-    }
+        int IS_PRESENT= 1;
+        double empCheck = (Math.random() * 10) % 2;
+         if (empCheck == IS_PRESENT) {
+             System.out.println("employee is present");
+         }else{
+             System.out.println("employee is absent");
+         }
+
+        }
+
 }
