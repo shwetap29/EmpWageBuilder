893e0eb7afca71cd62b20595039b13517cf7bed4
shwetap29 <shwetaprasad48@gmail.com>
2021-05-20 08:15:32 +0530

    added welcome to emp wage computation program

---
+++ src/main/java/com/bridgelabz/EmpWagebuilder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmpWagebuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to emp wage computation program");
+    }
+}
